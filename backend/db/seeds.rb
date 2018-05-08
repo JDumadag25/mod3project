6d339c8b53ea537ad6b20823fc9a56d2d9bdc960
JDumadag25 <justin.dumadag@gmail.com>
2018-05-08 17:04:19 -0400

    attempt at google places

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,3 +1,4 @@
+require 'Pry'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+apikey = File.open("backend/googleapikey.txt", "r").first
+
+places = RestClient.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=#{apikey}"
+places_hash = JSON.parse(places)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-fetch('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyC5bFY4wSd9ogZR7wxFjkFdJz3SvPPVO18')
+fetch('')
 .then(res => res.json())
 .then(json => console.log(json))
 
