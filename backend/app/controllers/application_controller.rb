3325d7582db17f7b322a353690b73c0a3321760d
JDumadag25 <justin.dumadag@gmail.com>
2018-05-09 09:39:50 -0400

    again

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::API
 
-# response = RestClient::Request.execute(
-# method: :get,
-# url: 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyC5bFY4wSd9ogZR7wxFjkFdJz3SvPPVO18',
-#
-# )
 
 
 apikey = File.open("backend/googleapikey.txt", "r").first
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,4 +1,3 @@
-require 'Pry'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,10 +5,3 @@ require 'Pry'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-apikey = File.open("backend/googleapikey.txt", "r").first
-search =
-
-places = RestClient.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{search}=#{apikey}"
-binding.pry
-places_hash = JSON.parse(places)
